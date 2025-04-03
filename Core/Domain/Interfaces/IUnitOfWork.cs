4c57fe1f48e34534569022e85ed35587c60ee809
abdulrahman <abdulrahmn398@outlook.com>
2025-04-03 18:32:33 +0200

    Add IUnitOfWork interface and UnitOfWork implementation
    Introduces the `IUnitOfWork` interface in the `Domain.Interfaces` namespace, defining methods for saving changes asynchronously and retrieving generic repositories. Implements the `UnitOfWork` class in the `Persistence.Repositories` namespace, which utilizes `AppDbContext` to fulfill the interface methods.

---
+++ Core/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync();
+    IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+        where TEntity : BaseEntity<TKey>;
+}
---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Persistence.Data;
+
+namespace Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _dbContext;
+
+    public UnitOfWork(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> SaveChangesAsync()
+        => await _dbContext.SaveChangesAsync();
+
+    public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+        where TEntity : BaseEntity<TKey>
+        => new GenericRepository<TEntity, TKey>(_dbContext);
+}
