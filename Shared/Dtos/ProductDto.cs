fc1d3cad42f60b918cd0d62bc2d0a40e66cba493
abdulrahman <abdulrahmn398@outlook.com>
2025-04-06 16:11:32 +0200

    Add DTOs and IProductService interface
    - Introduced `IProductService` interface in the `Abstractions` namespace.
    - Added `ProductBrandDto`, `ProductDto`, and `ProductTypeDto` records in the `Shared.Dtos` namespace.
    - `ProductBrandDto` includes properties for `Id` and `Name`.
    - `ProductDto` includes properties for `Id`, `Name`, `Description`, `ImageUrl`, `Price`, `ProductBrand`, and `ProductType`.
    - `ProductTypeDto` includes properties for `Id` and `Name`.

---
+++ Shared/Dtos/ProductDto.cs
@@ -0,0 +1,42 @@
+﻿namespace Shared.Dtos;
+
+/// <summary>
+/// Represents a data transfer object for a product.
+/// </summary>
+public record ProductDto
+{
+    /// <summary>
+    /// Gets or sets the identifier of the entity.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the product.
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the description of the product.
+    /// </summary>
+    public string Description { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the image URL of the product.
+    /// </summary>
+    public string ImageUrl { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the price of the product.
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Gets or sets the brand of the product.
+    /// </summary>
+    public string ProductBrand { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the type of the product.
+    /// </summary>
+    public string ProductType { get; set; } = null!;
+}
---
+++ Shared/Dtos/ProductTypeDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Dtos;
+
+/// <summary>
+/// Represents a data transfer object for a product type.
+/// </summary>
+public record ProductTypeDto
+{
+    /// <summary>
+    /// Gets or sets the identifier of the product type.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the product type.
+    /// </summary>
+    public string Name { get; set; } = null!;
+}
